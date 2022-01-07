f33112e1cd2e3d525cabb8112cd94241238789b9
Louis Charette <charette.louis@gmail.com>
2022-01-07 14:06:09 -0500

    Add AdminLTE config

---
+++ app/config/default.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * UserFrosting AdminLTE Theme (http://www.userfrosting.com)
+ *
+ * @link      https://github.com/userfrosting/theme-adminlte
+ * @copyright Copyright (c) 2021 Alexander Weissman & Louis Charette
+ * @license   https://github.com/userfrosting/theme-adminlte/blob/master/LICENSE (MIT License)
+ */
+
+/*
+ * AdminLTE Theme Configuration
+ */
+return [
+    /**
+     * ----------------------------------------------------------------------
+     * Site Settings
+     * ----------------------------------------------------------------------
+     * Add AdminLTE skin color config
+     */
+    'site' => [
+        // AdminLTE skin color
+        // See https://adminlte.io/themes/AdminLTE/documentation/index.html#layout
+        'AdminLTE' => [
+            'skin' => 'blue',
+        ],
+    ],
+];
